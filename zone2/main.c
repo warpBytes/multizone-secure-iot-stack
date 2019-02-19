d954fdcfea5a05f300622926b84357aa9fb76aba
Boran Car <boran.car@gmail.com>
2019-02-19 02:22:04 +0000

    Move eccSign and key to zone3
    Zone2 no longer has the private key and obtains a signature by asking
    zone3 to sign for it.

--- zone2/main.c
+++ zone2/main.c
@@ -338,20 +338,6 @@ static const char cert_der[] = {
   0xdf, 0xb0, 0xac
 };
 
-static const char key_der[] = {
-  0x30, 0x77, 0x02, 0x01, 0x01, 0x04, 0x20, 0x09, 0x5f, 0xdd, 0x40, 0x49,
-  0x93, 0x86, 0x7d, 0x4c, 0x97, 0x8e, 0x49, 0x84, 0xdb, 0x00, 0xfe, 0x5f,
-  0xb4, 0x71, 0x1d, 0xd6, 0x1c, 0x27, 0x6e, 0xa2, 0x89, 0x03, 0xc3, 0xf0,
-  0x8b, 0x50, 0xe0, 0xa0, 0x0a, 0x06, 0x08, 0x2a, 0x86, 0x48, 0xce, 0x3d,
-  0x03, 0x01, 0x07, 0xa1, 0x44, 0x03, 0x42, 0x00, 0x04, 0xf7, 0xde, 0x4e,
-  0x14, 0x5f, 0xbd, 0x6f, 0x4f, 0xea, 0xb4, 0x9b, 0x56, 0x44, 0x05, 0x1b,
-  0x9a, 0x3f, 0x0a, 0x5f, 0x02, 0x2a, 0x32, 0x5f, 0x40, 0xea, 0xcd, 0xee,
-  0x05, 0x90, 0x2e, 0xc3, 0x1a, 0xa0, 0xbb, 0x56, 0xd7, 0xcf, 0xd9, 0x8d,
-  0x6e, 0x8c, 0xf5, 0xe0, 0x0c, 0xff, 0x06, 0xdd, 0xf5, 0x02, 0x62, 0xd6,
-  0xed, 0x99, 0xd1, 0x4c, 0xad, 0xfd, 0xd8, 0x47, 0x35, 0x49, 0x86, 0x45,
-  0x06
-};
-
 void cb_tls(uint16_t ev, struct pico_socket *s)
 {
     struct pico_socket *sock_tls;
@@ -418,10 +404,7 @@ unsigned int my_rng_seed_gen(void)
     return pico_rand();
 }
 
-#include <wolfssl/wolfcrypt/ecc.h>
 
-WC_RNG rng;
-ecc_key eccKey;
 
 static int eccSign(WOLFSSL* ssl,
        const unsigned char* in, unsigned int inSz,
@@ -429,7 +412,49 @@ static int eccSign(WOLFSSL* ssl,
        const unsigned char* keyDer, unsigned int keySz,
        void* ctx)
 {
-    return wc_ecc_sign_hash(in, inSz, out, outSz, &rng, &eccKey);
+    int msg[4];
+    int i, ret;
+
+    msg[0] = 1;
+    msg[1] = inSz;
+    msg[2] = *outSz;
+    msg[3] = 0;
+    ECALL_SEND(3, msg);
+    ECALL_YIELD();
+
+    i = 0;
+    while (i < inSz) {
+        int len;
+        if (inSz - i > 12)
+            len = 12;
+        else
+            len = inSz - i;
+
+        msg[0] = len;
+        memcpy(&msg[1], in + i, len);
+        ECALL_SEND(3, msg);
+        ECALL_YIELD();
+        i += len;
+    }
+
+    do {
+        ECALL_YIELD();
+        ECALL_RECV(3, msg);
+    } while(msg[0] != 1);
+    ret = msg[1];
+    *outSz = msg[2];
+
+    i = 0;
+    while (i < *outSz) {
+        ECALL_YIELD();
+        ECALL_RECV(3, msg);
+        if (msg[0] > 0) {
+            memcpy(out+i, &msg[1], msg[0]);
+            i += msg[0];
+        }
+    }
+
+    return ret;
 }
 
 static int wolfRecv(WOLFSSL *ssl, char *buf, int sz, void *ctx)
@@ -461,7 +486,6 @@ static int wolfSend(WOLFSSL *ssl, char *buf, int sz, void *ctx)
 }
 
 int main(int argc, char *argv[]){
-    unsigned int idx = 0;
     uint16_t telnet_port = short_be(23);
     uint16_t tls_port = short_be(443);
     uint32_t yes = 1;
@@ -535,24 +559,6 @@ int main(int argc, char *argv[]){
     ret = wolfSSL_CTX_use_certificate_buffer(ctx, cert_der, sizeof(cert_der), SSL_FILETYPE_ASN1);
     if (ret != SSL_SUCCESS) {
         printf("Could not load certificate!\n");
-        return -1;
-    }
-
-    ret = wc_InitRng(&rng);
-    if (ret != 0) {
-        printf("Could not init rng!\n");
-        return -1;
-    }
-
-    ret = wc_ecc_init(&eccKey);
-    if (ret != 0) {
-        printf("Could not init ECC key!\n");
-        return -1;
-    }
-
-    ret = wc_EccPrivateKeyDecode(key_der, &idx, &eccKey, sizeof(key_der));
-    if (ret != 0) {
-        printf("Could not decode private ECC key!\n");
         return -1;
     }
 
